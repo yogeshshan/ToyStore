2648ba3e12318e76c27f677bd5ae74e6c3a47544
yogeshshan <60909956+yogeshshan@users.noreply.github.com>
2023-07-18 03:56:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
-     terraform {
-       backend "remote" {
-         organization = "yogi_12345"
-         workspaces {
-           name = "ToyStore"
-        }
-      }
-    }
+terraform {
+  backend "remote" {
+    organization = "yogi_12345"
 
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-       }
+    workspaces {
+      name = "ToyStore"
     }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "An example resource that does nothing!"
+  }
+}
